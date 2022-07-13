b67c7a204bf79f4dc6aa1bf863fe7b32b025c48d
Rayenire <aeristarg@gmail.com>
2022-07-13 21:18:27 +0300

    Add task04

---
+++ task04/Program.cs
@@ -0,0 +1,9 @@
+﻿int a0 = 0, a1 = 1, a2, n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    a2 = a1 + a0;
+    a0 = a1;
+    a1 = a2;
+    Console.Write(a2);
+    Console.Write(" ");
+}

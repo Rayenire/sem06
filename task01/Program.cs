1c6b355b94ed07e075c156773ffc1d09920d7d4f
Rayenire <aeristarg@gmail.com>
2022-07-13 20:25:50 +0300

    Add task01

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+var random = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(100, 999);
+}
+Console.WriteLine();
+Console.WriteLine("[" + string.Join(",", array) + "]");
+for(var i = 0; i < array.Length/2; i++)
+{
+    (array[i], array[array.Length - (i + 1)]) = (array[array.Length - (i + 1)], array[i]);
+}
+Console.WriteLine();
+Console.WriteLine("[" + string.Join(",", array) + "]");

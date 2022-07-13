2172443945ff1eeaac7fa03129142e6ff100c32d
Rayenire <aeristarg@gmail.com>
2022-07-13 20:59:51 +0300

    Add task03

---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿int  count = 0, x, n = Convert.ToInt32(Console.ReadLine());
+List<int> array = new List<int>();
+while (n > 0)
+{
+    x = n % 2;
+    array.Add(x);
+    count++;
+    //Console.Write(x);
+    n /= 2;
+}
+for (int i = count - 1; i >= 0; i--)
+{
+    Console.Write(array[i]);
+}

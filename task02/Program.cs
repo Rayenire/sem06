45356a6586c10cd87b9ec7ca9f1ea9e206bdc242
Rayenire <aeristarg@gmail.com>
2022-07-13 20:37:19 +0300

    Add task02

---
+++ task02/Program.cs
@@ -0,0 +1,11 @@
+﻿string f(int a, int b, int c)
+{
+    if ((a + b) > c && (b + c) > a && (c + a) > b)
+        return "Triangle can exist";
+    return "Triangle can't exist";    
+}
+int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a, b, c));
